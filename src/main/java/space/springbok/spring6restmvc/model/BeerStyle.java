db9824803c5dd31f6389527e7a840fb943c635de
johnspangenberg <dev@springbok.space>
2025-10-17 16:11:02 +0200

    Add Beer and BeerStyle models, update Java version to 24

---
+++ src/main/java/space/springbok/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,10 @@
+package space.springbok.spring6restmvc.model;
+
+/**
+ *
+ * @author John Spangenberg
+ */
+public enum BeerStyle {
+
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
